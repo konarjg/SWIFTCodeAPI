45f3bae20fdddfdd4c08ff2caa1311adc664ce4c
konarjg <konarskikrzysztof1@gmail.com>
2025-03-12 23:27:16 +0100

    Finished first iteration of SWIFT code parser

--- src/main/java/com/github/konarjg/SWIFTCodeAPI/component/SwiftCodeParserComponent.java
+++ src/main/java/com/github/konarjg/SWIFTCodeAPI/component/SwiftCodeParserComponent.java
@@ -1,9 +1,18 @@
 package com.github.konarjg.SWIFTCodeAPI.component;
 
 import com.github.konarjg.SWIFTCodeAPI.entity.SwiftCode;
+import org.apache.poi.EmptyFileException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -12,11 +21,85 @@ public class SwiftCodeParserComponent implements SwiftCodeParser {
 
     @Override
     public SwiftCode parseCode(HashMap<String, String> data) {
-        return null;
+        String countryISO2 = data.get("countryISO2");
+        String swiftCode = data.get("swiftCode");
+        String bankName = data.get("bankName");
+        String address = data.get("address");
+        String countryName = data.get("countryName");
+        boolean isHeadquarter = swiftCode.endsWith("XXX");
+
+        SwiftCode result = new SwiftCode();
+        result.setCountryISO2(countryISO2);
+        result.setSwiftCode(swiftCode);
+        result.setBankName(bankName);
+        result.setAddress(address);
+        result.setCountryName(countryName);
+        result.setHeadquarter(isHeadquarter);
+        result.setHeadquartersCode(null);
+        result.setBranches(new ArrayList<>());
+
+        return result;
+    }
+
+    private List<SwiftCode> readSpreadsheet(Workbook workbook) {
+        List<SwiftCode> result = new ArrayList<>();
+        Sheet sheet = workbook.getSheetAt(0);
+
+        for (Row row : sheet) {
+            if (row.getRowNum() == 0) {
+                continue;
+            }
+
+            if (row.getCell(0).getStringCellValue().isEmpty()) {
+                break;
+            }
+
+            HashMap<String, String> data = new HashMap<>();
+
+            data.put("countryISO2", row.getCell(0).getStringCellValue());
+            data.put("swiftCode", row.getCell(1).getStringCellValue());
+            data.put("bankName", row.getCell(3).getStringCellValue());
+            data.put("address", row.getCell(4).getStringCellValue());
+            data.put("countryName", row.getCell(7).getStringCellValue());
+
+            SwiftCode code = parseCode(data);
+
+            result.add(code);
+        }
+
+        return result;
     }
 
     @Override
     public List<SwiftCode> parseCodes(File file) {
-        return List.of();
+        if (file == null || !file.exists()) {
+            return List.of();
+        }
+
+        try (FileInputStream inputStream = new FileInputStream(file);
+             Workbook workbook = WorkbookFactory.create(inputStream)) {
+
+            List<SwiftCode> result = readSpreadsheet(workbook);
+
+            for (SwiftCode code : result) {
+                SwiftCode parent = result.stream()
+                        .filter(swiftCode -> swiftCode.isHeadquarter() && swiftCode.getSwiftCode()
+                                .substring(0, 8)
+                                .equals(code.getSwiftCode().substring(0, 8)))
+                        .findFirst().orElse(null);
+
+                if (parent != null) {
+                    if (parent.getBranches() == null) {
+                        parent.setBranches(new ArrayList<>());
+                    }
+
+                    parent.getBranches().add(code);
+                }
+            }
+
+            return result;
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
     }
 }
--- src/test/java/com/github/konarjg/SWIFTCodeAPI/SwiftCodeParserComponentTests.java
+++ src/test/java/com/github/konarjg/SWIFTCodeAPI/SwiftCodeParserComponentTests.java
@@ -3,14 +3,23 @@ package com.github.konarjg.SWIFTCodeAPI;
 import com.github.konarjg.SWIFTCodeAPI.component.SwiftCodeParser;
 import com.github.konarjg.SWIFTCodeAPI.component.SwiftCodeParserComponent;
 import com.github.konarjg.SWIFTCodeAPI.entity.SwiftCode;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestTemplate;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -18,6 +27,20 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class SwiftCodeParserComponentTests {
+    private File createTempSpreadsheet(int id) {
+        String path = "test" + id + ".xlsx";
+
+        try(InputStream stream = this.getClass().getClassLoader().getResourceAsStream(path)) {
+            File file = Files.createTempFile("test" + id, ".xlsx").toFile();
+            file.deleteOnExit();
+
+            Files.copy(stream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            return file;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
     @Test
     public void parseCode_whenCodeEndsWithXXX_shouldParseHeadquartersCode() {
         //Arrange
@@ -101,9 +124,7 @@ public class SwiftCodeParserComponentTests {
     public void parseCodes_whenFileContainsValidCodes_shouldReturnNonEmptyList() throws IOException {
         //Arrange
         SwiftCodeParser parser = new SwiftCodeParserComponent();
-        File file = Files.createTempFile("test", ".xlsx").toFile();
-        Files.writeString(file.toPath(), "AL\tAAISALTRXXX\tBIC11\tUNITED BANK OF ALBANIA SH.A\tHYRJA 3 RR. DRITAN HOXHA ND. 11 TIRANA, TIRANA, 1023\tTIRANA\tALBANIA\tEurope/Tirane");
-        file.deleteOnExit();
+        File file = createTempSpreadsheet(0);
 
         //Act
         List<SwiftCode> result = parser.parseCodes(file);
@@ -116,9 +137,7 @@ public class SwiftCodeParserComponentTests {
     public void parseCodes_whenFileContainsHeadquarterCode_shouldReturnListWithHeadquarterCode() throws IOException {
         //Arrange
         SwiftCodeParser parser = new SwiftCodeParserComponent();
-        File file = Files.createTempFile("test", ".xlsx").toFile();
-        Files.writeString(file.toPath(), "AL\tAAISALTRXXX\tBIC11\tUNITED BANK OF ALBANIA SH.A\tHYRJA 3 RR. DRITAN HOXHA ND. 11 TIRANA, TIRANA, 1023\tTIRANA\tALBANIA\tEurope/Tirane");
-        file.deleteOnExit();
+        File file = createTempSpreadsheet(0);
 
         //Act
         List<SwiftCode> result = parser.parseCodes(file);
@@ -131,9 +150,7 @@ public class SwiftCodeParserComponentTests {
     public void parseCodes_whenFileContainsBranchCode_shouldReturnListWithBranchCode() throws IOException {
         //Arrange
         SwiftCodeParser parser = new SwiftCodeParserComponent();
-        File file = Files.createTempFile("test", ".xlsx").toFile();
-        Files.writeString(file.toPath(), "AL\tAAISALTR\tBIC11\tUNITED BANK OF ALBANIA SH.A\tHYRJA 3 RR. DRITAN HOXHA ND. 11 TIRANA, TIRANA, 1023\tTIRANA\tALBANIA\tEurope/Tirane");
-        file.deleteOnExit();
+        File file = createTempSpreadsheet(1);
 
         //Act
         List<SwiftCode> result = parser.parseCodes(file);
@@ -146,13 +163,7 @@ public class SwiftCodeParserComponentTests {
     public void parseCodes_whenFileContainsBranchCodeWithEightCharacterMatchingPreviousHeadquarter_shouldReturnListWithHeadquarterCodeThatHasBranchCode() throws IOException {
         //Arrange
         SwiftCodeParser parser = new SwiftCodeParserComponent();
-        StringBuilder builder = new StringBuilder();
-        builder.append("AL\tAAISALTRXXX\tBIC11\tUNITED BANK OF ALBANIA SH.A\tHYRJA 3 RR. DRITAN HOXHA ND. 11 TIRANA, TIRANA, 1023\tTIRANA\tALBANIA\tEurope/Tirane\n");
-        builder.append("AL\tAAISALTR\tBIC11\tUNITED BANK OF ALBANIA SH.A\tHYRJA 3 RR. DRITAN HOXHA ND. 11 TIRANA, TIRANA, 1023\tTIRANA\tALBANIA\tEurope/Tirane\n");
-
-        File file = Files.createTempFile("test", ".xlsx").toFile();
-        Files.writeString(file.toPath(), builder.toString());
-        file.deleteOnExit();
+        File file = createTempSpreadsheet(2);
 
         //Act
         List<SwiftCode> result = parser.parseCodes(file);
@@ -165,18 +176,13 @@ public class SwiftCodeParserComponentTests {
     public void parseCodes_whenFileContainsHeadquarterCodeWithEightCharacterMatchingPreviousHeadquarter_shouldReturnListWithHeadquarterCodeThatHasBranchHeadquarterCode() throws IOException {
         //Arrange
         SwiftCodeParser parser = new SwiftCodeParserComponent();
-        StringBuilder builder = new StringBuilder();
-        builder.append("AL\tAAISALTRXXX\tBIC11\tUNITED BANK OF ALBANIA SH.A\tHYRJA 3 RR. DRITAN HOXHA ND. 11 TIRANA, TIRANA, 1023\tTIRANA\tALBANIA\tEurope/Tirane\n");
-        builder.append("AL\tAAISALTRBXXX\tBIC11\tUNITED BANK OF ALBANIA SH.A\tHYRJA 3 RR. DRITAN HOXHA ND. 11 TIRANA, TIRANA, 1023\tTIRANA\tALBANIA\tEurope/Tirane\n");
-
-        File file = Files.createTempFile("test", ".xlsx").toFile();
-        Files.writeString(file.toPath(), builder.toString());
-        file.deleteOnExit();
+        File file = createTempSpreadsheet(3);
 
         //Act
         List<SwiftCode> result = parser.parseCodes(file);
 
         //Assert
+        System.out.println(result.get(0).getSwiftCode());
         assertTrue(result.stream().anyMatch(code -> code.isHeadquarter() && !code.getBranches().isEmpty()));
     }
 }
